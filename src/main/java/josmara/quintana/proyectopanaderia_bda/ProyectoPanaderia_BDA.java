9cfe93e9f71654629aa1cc07a36ee749e2dbc793
josmaraquintana <josmaraalexaq@gmail.com>
2026-02-18 15:42:52 -0700

    Initial commit

---
+++ src/main/java/josmara/quintana/proyectopanaderia_bda/ProyectoPanaderia_BDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package josmara.quintana.proyectopanaderia_bda;
+
+/**
+ *
+ * @author josma
+ */
+public class ProyectoPanaderia_BDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hola chicos hay que chambiar");
+    }
+}
