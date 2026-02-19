01667b140b45a4e29547b0cc3cbbf2226f9bbd9f
josmaraquintana <josmaraalexaq@gmail.com>
2026-02-18 22:50:43 -0700

    feat: implementación paquetes validaciones y GUIs
    Prueba de que si se suba todo a GitHub subiendo dos paquetes

--- src/main/java/Validadores/Validaciones.java
+++ src/main/java/Validadores/Validaciones.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package josmara.quintana.proyectopanaderia_bda;
+package Validadores;
 
 /**
  *
  * @author josma
  */
-public class prueba {
+public class Validaciones {
+    
 }
